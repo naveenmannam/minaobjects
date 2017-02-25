60cf8db6e40af3fa08b5d4581b1de69980137fcc
Naveen Mannam <naveen.mananm@hotmail.com>
2017-02-25 13:30:54 -0500

    Added new js file

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Global object prototype
+var firstname = "Minny";
+var lastname = "Susmitha";
+
+
+var person = {
+	firstname : "Naveen",
+	lastname : "Mannam",
+	age: 23
+};
+
+Object.prototype.greet = function () {
+	console.log("Hello, This is under global prototype.");
+};
+
+
+
+Object.prototype.greets = function (firstname, lastname) {
+	console.log("Hello, " + this.firstname + " " + this.lastname + " This is under global prototype.");
+};
+
+
+person.greet();
+person.greets();
+greet();
+greets();
